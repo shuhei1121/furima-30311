855029cafb64e841fc8d202b71189e10aa96671f
shuhei1121 <72679428+shuhei1121@users.noreply.github.com>
2021-01-05 19:23:20 +0900

    ActiveHash実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+  has_one :purchase
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
+  validates :
+  validates :categpry_id, :status_id, :delivery_fee_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1 } 
 end
