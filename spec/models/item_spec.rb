95d780b32b618a3a5c5714efddffd72cb5e87d1f
shuhei1121 <72679428+shuhei1121@users.noreply.github.com>
2021-01-10 15:02:23 +0900

    rubocop実行

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe Item, type: :model do
     @item = FactoryBot.build(:item)
   end
   describe '商品の出品' do
-    context "商品の出品ができる場合" do
+    context '商品の出品ができる場合' do
       it '必須項目を全て入力すれば出品できる' do
         expect(@item).to be_valid
       end
     end
-    
-    context "商品の出品できない場合" do
+
+    context '商品の出品できない場合' do
       it 'imageが空では出品できない' do
         @item.image = nil
         @item.valid?
@@ -115,7 +115,7 @@ RSpec.describe Item, type: :model do
       end
 
       it 'priceが半角英語だけでは出品できない' do
-        @item.price = "test"
+        @item.price = 'test'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
